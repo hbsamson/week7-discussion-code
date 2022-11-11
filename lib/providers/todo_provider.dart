61fe1725ba4c4b96c317cf76b31cdc5c756277dd
hbsamson <hbsamson@up.edu.ph>
2022-11-11 16:48:20 +0800

    Create Network Calls

--- lib/providers/todo_provider.dart
+++ lib/providers/todo_provider.dart
@@ -5,54 +5,69 @@
 */
 
 import 'package:flutter/material.dart';
+import 'package:week7_networking_discussion/api/todo_api.dart';
 import 'package:week7_networking_discussion/models/todo_model.dart';
 
 class TodoListProvider with ChangeNotifier {
-  List<Todo> _todoList = [
-    Todo(
-      completed: true,
-      userId: 1,
-      id: 1,
-      title: "Grocery",
-    ),
-    Todo(
-      completed: true,
-      userId: 1,
-      id: 2,
-      title: "Bills",
-    ),
-    Todo(
-      completed: false,
-      userId: 1,
-      id: 3,
-      title: "Walk dog",
-    ),
-  ];
-
+  // List<Todo> _todoList = [
+  //   Todo(
+  //     completed: true,
+  //     userId: 1,
+  //     id: 1,
+  //     title: "Grocery",
+  //   ),
+  //   Todo(
+  //     completed: true,
+  //     userId: 1,
+  //     id: 2,
+  //     title: "Bills",
+  //   ),
+  //   Todo(
+  //     completed: false,
+  //     userId: 1,
+  //     id: 3,
+  //     title: "Walk dog",
+  //   ),
+  // ];
   // getter
-  List<Todo> get todo => _todoList;
+  // List<Todo> get todo => _todoList;
+
+  late TodoAPI todoAPI;
+  late Future<List<Todo>> _todoList;
+
+  Future<List<Todo>> get todo => _todoList;
+
+  TodoListProvider() {
+    todoAPI = TodoAPI();
+    fetchTodos();
+  }
+
+  void fetchTodos() {
+    _todoList = todoAPI.fetchTodos();
+    notifyListeners();
+  }
 
   void addTodo(Todo item) {
-    _todoList.add(item);
+    // _todoList.add(item);
     notifyListeners();
   }
 
   void editTodo(int index, String newTitle) {
-    _todoList[index].title = newTitle;
+    // _todoList[index].title = newTitle;
     notifyListeners();
   }
 
   void deleteTodo(String title) {
-    for (int i = 0; i < _todoList.length; i++) {
-      if (_todoList[i].title == title) {
-        _todoList.remove(_todoList[i]);
-      }
-    }
+    // for (int i = 0; i < _todoList.length; i++) {
+    //   if (_todoList[i].title == title) {
+    //     _todoList.remove(_todoList[i]);
+    //   }
+    // }
     notifyListeners();
   }
 
   void toggleStatus(int index, bool status) {
-    _todoList[index].completed = status;
+    // _todoList[index].completed = status;
     notifyListeners();
   }
 }
